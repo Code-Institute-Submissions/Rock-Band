7d004cdecff4723a5bf3c240783ba3ab43e0e696
Karol Sliwka <hello@abovewave.co.uk>
2019-02-09 15:06:59 +0000

    scirpt moved from html to outside js file

--- script/script.js
+++ script/script.js
@@ -1,4 +1,21 @@
-function sowaht() {
-    var news_email = document.getElementById("footer-email").value;
-    console.log(news_email);
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    }
+    else {
+        document.getElementById("myBtn").style.display = "none";
+    }
 }
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+// menu toggle collaps script from - https://www.codeply.com/go/XtiWqN3lGn //
+$('.navbar-nav>li>a').on('click', function($) {
+    $('.navbar-collapse').collapse('hide');
+});
